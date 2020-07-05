c9f5fa0a4293f5bbede93233929b6477d7883c27
Christian McCabe <seeward@gmail.com>
2020-07-05 13:36:03 +0200

    add right attack healing

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     codeplant.jesus.playAnimation(codeplant.jesus.AnimationTypes.IdleLeft)
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    codeplant.jesus.playAnimation(codeplant.jesus.AnimationTypes.HealRightWithStaff)
+    pause(400)
+    explosions.sendHealingBolt(tes.sprite, 2)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     codeplant.jesus.playAnimation(codeplant.jesus.AnimationTypes.HealLeftWithStaff)
     pause(400)
-    explosions.sendHealingBolt(tes.sprite)
+    explosions.sendHealingBolt(tes.sprite,1)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     codeplant.jesus.playAnimation(codeplant.jesus.AnimationTypes.IdleRight)
 })
 let tes: Jesus = null
-scene.setBackgroundColor(9)
+scene.setBackgroundColor(15)
 tes = codeplant.jesus.create(SpriteKind.Player)
-tes.sprite.setPosition(141, 13)
+tes.sprite.setPosition(8, 9)
+codeplant.jesus.playAnimation(codeplant.jesus.AnimationTypes.IdleRight)
 controller.moveSprite(tes.sprite)
